176387f44147cbbc9651365ff24f742d26fb0cef
TrojanB <qwertroya@gmail.com>
2023-02-03 08:45:58 +0100

    grzybki

--- grzyby.php
+++ grzyby.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -18,25 +19,42 @@
         </div>
 
         <div id="lewy">
-            <?php
-            $baza = mysqli_connect("localhost", "root", "", "3pir_dane2");
+           <?php
+           $db = mysqli_connect('localhost', 'root', '', '3pir_dane2');
+            $kwerenda = "SELECT nazwa_pliku, potoczna FROM grzyby WHERE jadalny=true;";
+            $wynik = mysqli_query($db, $kwerenda);
 
-            $kwerenda = "SELECT nazwa_pliku, potoczna FROM grzyby;";
-
-                mysqli_querry($baza, $kwerenda);
-
-                while($grzybek = mysqli_querry($baza, $kwerenda)){
-                    echo "<img src = ./pliki5.$grzybek[0] name=$grzybek[1]>";
-                }
-
-                mysqli_close($baza);
-            ?>
+            while($kw = mysqli_fetch_row($wynik)){
+                echo "<img src=./pliki5/$kw[0] title=$kw[1]>";
+            }
+            mysqli_close($db);
+           ?>
         </div>
         <div id="prawy">
             <h2>Grzyby jadalne</h2>
-            <?php ?>
+            <?php
+            $db = mysqli_connect('localhost', 'root', '', '3pir_dane2');
+            $kwerenda = " SELECT nazwa, potoczna FROM grzyby WHERE jadalny=true;";
+            $wynik = mysqli_query($db, $kwerenda);
+
+            while($kw = mysqli_fetch_row($wynik)){
+                echo "<p>$kw[0] ($kw[1])</p>";
+            }
+            mysqli_close($db);?>
             <h2>Polecamy do sosów</h2>
-            <?php ?>
+            <?php
+
+            $db = mysqli_connect('localhost', 'root', '', '3pir_dane2');
+            $kwerenda = " SELECT grzyby.nazwa, grzyby.potoczna, rodzina.nazwa FROM grzyby INNER JOIN rodzina ON grzyby.rodzina_id = rodzina.id WHERE jadalny=1;";
+            $wynik = mysqli_query($db, $kwerenda);
+
+            echo "<ol>";
+            while ($kw = mysqli_fetch_row($wynik)) {
+                echo "<li>$kw[0]($kw[1]), rodzina: $kw[2]</li>";
+
+            }
+            echo "</ol>";
+            ?>
         </div>
 
         <footer><p>Autor: Trojan Bogacki 3pir</p></footer>
